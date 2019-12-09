f661e7c9b222ced451c55e2778aaeaecf93ceb15
tSimoes06 <tiago.scr06@gmail.com>
2019-12-09 18:53:06 -0300

    Adicionando arquivo main

---
+++ analise_ruido.py
@@ -0,0 +1,7 @@
+import panda as pd 
+
+## Cada tabela 
+tabelao_EBA_D5 = pd.read_csv('./Pedestal (Ruido)/EBA_D5_pedestal_statistics.txt')
+tabelao_EBA_D6 = pd.read_csv('./Pedestal (Ruido)/EBA_D6_pedestal_statistics.txt')
+tabelao_EBC_D5 = pd.read_csv('./Pedestal (Ruido)/EBC_D5_pedestal_statistics.txt')
+tabelao_EBC_D6 = pd.read_csv('./Pedestal (Ruido)/EBC_D6_pedestal_statistics.txt')
