0905bcd65258bde331ae1edb6dd869876310cbe1
Lucas P <lucasperovani@gmail.com>
2019-12-11 12:27:29 -0300

    :P

--- analise_ruido.py
+++ analise_ruido.py
@@ -4,19 +4,23 @@ import scipy.stats as sps
 from scipy import optimize
 import numpy as np
 
+
+QUANTMODULOS = 4
+
+
 def gaussiana(x,media,variancia):
     g = (1/(np.sqrt(2*np.pi*variancia)))*np.exp(-(1/(2*variancia))*((x-media))**2)
     return g
 
 
-def plotaHist(tabelao, nome, quantBins=7):
+def plotaHist(tabelao, nome, quantBins=5):
     plt.figure(figsize=(10,8))
 
     print (" ")
 
     print ("###########  " + nome + "  ###########")
 
-    ruido = np.linspace(20, 45, 300)
+    ruido = np.linspace(10, 50, 300)
 # 4 graus de liberdade
     # Fita os dados
     (mu, sigma) = sps.norm.fit(tabelao)
@@ -40,18 +44,27 @@ def plotaHist(tabelao, nome, quantBins=7):
     plt.xlabel = 'Intervalo do Ruido'
     plt.ylabel = 'Quantidade nos Bins'
     plt.title(r'$\mathrm{Histograma\ do\ Ruido :}\ \ mu=%.3f,\ \sigma=%.3f,\ histmu=%.3f,\ histsigma=%.3f$' %(mu, sigma,parametros_otimos[0],parametros_otimos[1]))
-    plt.savefig(f'./Figuras/{nome}')
+    #plt.savefig(f'./Figuras/{nome}')
+    plt.savefig(f'/home/lucas/Documents/UFRJ/ITM/ITM_Ruido-master/Figuras/{nome}')
     plt.close()
 
-    print("Chi quadrado: " + str((sps.chisquare(histEst,gausEsp, 2)[0])) + "%     seila: " + str(sps.chisquare(histEst,gausEsp, 2)[1]))
+    chiquadrado, confLevel = sps.chisquare(histEst,gausEsp, 2)
+
+    print("Chi quadrado: " + str(chiquadrado) + "     CL: " + str(confLevel*100)+"%")
     pass
 
 
 ## Cada tabela 
-tabelao_EBA_D5 = pd.read_csv('./Pedestal (Ruido)/EBA_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-tabelao_EBA_D6 = pd.read_csv('./Pedestal (Ruido)/EBA_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-tabelao_EBC_D5 = pd.read_csv('./Pedestal (Ruido)/EBC_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-tabelao_EBC_D6 = pd.read_csv('./Pedestal (Ruido)/EBC_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+#tabelao_EBA_D5 = pd.read_csv('./Pedestal (Ruido)/EBA_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+#tabelao_EBA_D6 = pd.read_csv('./Pedestal (Ruido)/EBA_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+#tabelao_EBC_D5 = pd.read_csv('./Pedestal (Ruido)/EBC_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+#tabelao_EBC_D6 = pd.read_csv('./Pedestal (Ruido)/EBC_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+
+tabelao_EBA_D5 = pd.read_csv('/home/lucas/Documents/UFRJ/ITM/ITM_Ruido-master/Pedestal (Ruido)/EBA_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+tabelao_EBA_D6 = pd.read_csv('/home/lucas/Documents/UFRJ/ITM/ITM_Ruido-master/Pedestal (Ruido)/EBA_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+tabelao_EBC_D5 = pd.read_csv('/home/lucas/Documents/UFRJ/ITM/ITM_Ruido-master/Pedestal (Ruido)/EBC_D5_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+tabelao_EBC_D6 = pd.read_csv('/home/lucas/Documents/UFRJ/ITM/ITM_Ruido-master/Pedestal (Ruido)/EBC_D6_pedestal_statistics.txt', sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+
 
 #print (tabelao_EBA_D5.head())
 
@@ -65,30 +78,68 @@ Modulo Canal Sample1 Sample2 Sample3 Sample4 Sample5 Sample6 Sample7
 '''
 
 ## Separar os canais de EBA
-tabelao_EBA_D5_c0 =  tabelao_EBA_D5[(tabelao_EBA_D5['Canal'] == 0) and (tabelao_EBA_D5['Modulo'] == 0)]
-tabelao_EBA_D5_c1 =  tabelao_EBA_D5[tabelao_EBA_D5['Canal'] == 1 and tabelao_EBA_D5['Modulo'] == 0]
-tabelao_EBA_D6_c2 =  tabelao_EBA_D6[tabelao_EBA_D6['Canal'] == 2 and tabelao_EBA_D6['Modulo'] == 0]
-tabelao_EBA_D6_c3 =  tabelao_EBA_D6[tabelao_EBA_D6['Canal'] == 3 and tabelao_EBA_D5['Modulo'] == 0]
+tabelao_EBA_D5_c0 =  []
+tabelao_EBA_D5_c1 =  []
+tabelao_EBA_D6_c2 =  []
+tabelao_EBA_D6_c3 =  []
+
+tabelao_EBC_D5_c0 =  []
+tabelao_EBC_D5_c1 =  []
+tabelao_EBC_D6_c2 =  []
+tabelao_EBC_D6_c3 =  []
+
+for modulo in range (0,QUANTMODULOS+1):
+
+    vetoraux = tabelao_EBA_D5[(tabelao_EBA_D5['Canal'] == 0)]
+    tabelao_EBA_D5_c0.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBA_D5[(tabelao_EBA_D5['Canal'] == 1)]
+    tabelao_EBA_D5_c1.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBA_D6[(tabelao_EBA_D6['Canal'] == 2)]
+    tabelao_EBA_D6_c2.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBA_D6[(tabelao_EBA_D6['Canal'] == 3)]
+    tabelao_EBA_D6_c3.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+
+    vetoraux = tabelao_EBC_D5[(tabelao_EBC_D5['Canal'] == 0)]
+    tabelao_EBC_D5_c0.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBC_D5[(tabelao_EBC_D5['Canal'] == 1)]
+    tabelao_EBC_D5_c1.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBC_D6[(tabelao_EBC_D6['Canal'] == 2)]
+    tabelao_EBC_D6_c2.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+
+    vetoraux = tabelao_EBC_D6[(tabelao_EBC_D6['Canal'] == 3)]
+    tabelao_EBC_D6_c3.append(vetoraux[(vetoraux['Modulo'] == modulo)])
+    
+
+#tabelao_EBA_D5_c1 =  tabelao_EBA_D5[tabelao_EBA_D5['Canal'] == 1 and tabelao_EBA_D5['Modulo'] == 0]
+#tabelao_EBA_D6_c2 =  tabelao_EBA_D6[tabelao_EBA_D6['Canal'] == 2 and tabelao_EBA_D6['Modulo'] == 0]
+#tabelao_EBA_D6_c3 =  tabelao_EBA_D6[tabelao_EBA_D6['Canal'] == 3 and tabelao_EBA_D5['Modulo'] == 0]
 
 
 ### Separar os canais de EBC
-tabelao_EBC_D5_c0 =  tabelao_EBC_D5[tabelao_EBC_D5['Canal'] == 0 and tabelao_EBC_D5['Modulo'] == 0]
-tabelao_EBC_D5_c1 =  tabelao_EBC_D5[tabelao_EBC_D5['Canal'] == 1 and tabelao_EBC_D5['Modulo'] == 0]
-tabelao_EBC_D6_c2 =  tabelao_EBC_D6[tabelao_EBC_D6['Canal'] == 2 and tabelao_EBC_D5['Modulo'] == 0]
-tabelao_EBC_D6_c3 =  tabelao_EBC_D6[tabelao_EBC_D6['Canal'] == 3 and tabelao_EBC_D5['Modulo'] == 0]
+#tabelao_EBC_D5_c0 =  tabelao_EBC_D5[tabelao_EBC_D5['Canal'] == 0 and tabelao_EBC_D5['Modulo'] == 0]
+#tabelao_EBC_D5_c1 =  tabelao_EBC_D5[tabelao_EBC_D5['Canal'] == 1 and tabelao_EBC_D5['Modulo'] == 0]
+#tabelao_EBC_D6_c2 =  tabelao_EBC_D6[tabelao_EBC_D6['Canal'] == 2 and tabelao_EBC_D5['Modulo'] == 0]
+#tabelao_EBC_D6_c3 =  tabelao_EBC_D6[tabelao_EBC_D6['Canal'] == 3 and tabelao_EBC_D5['Modulo'] == 0]
 
 
 ## Plotar histograma
-for aux in range (1,8):
-    plotaHist(tabelao_EBA_D5_c0['Sample'+str(aux)],'Hist_EBA_D5_c0_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBA_D5_c1['Sample'+str(aux)],'Hist_EBA_D5_c1_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBA_D6_c2['Sample'+str(aux)],'Hist_EBA_D6_c2_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBA_D6_c3['Sample'+str(aux)],'Hist_EBA_D6_c3_Sample'+str(aux)+'_Modulo0-10bins')
+for sample in range (1,8):
+    for modulo in range (0,QUANTMODULOS+1):
+        plotaHist(tabelao_EBA_D5_c0[0]['Sample'+str(sample)],'Hist_EBA_D5_c0_Sample'+str(sample)+'_Modulo'+str(modulo)+'-10bins')
+        #plotaHist(tabelao_EBA_D5_c1['Sample'+str(sample)],'Hist_EBA_D5_c1_Sample'+str(sample)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBA_D6_c2['Sample'+str(sample)],'Hist_EBA_D6_c2_Sample'+str(sample)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBA_D6_c3['Sample'+str(sample)],'Hist_EBA_D6_c3_Sample'+str(sample)+'_Modulo0-10bins')
 
-    #plotaHist(tabelao_EBC_D5_c0['Sample'+str(aux)],'Hist_EBC_D5_c0_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBC_D5_c1['Sample'+str(aux)],'Hist_EBC_D5_c1_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBC_D6_c2['Sample'+str(aux)],'Hist_EBC_D6_c2_Sample'+str(aux)+'_Modulo0-10bins')
-    #plotaHist(tabelao_EBC_D6_c3['Sample'+str(aux)],'Hist_EBC_D6_c3_Sample'+str(aux)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBC_D5_c0['Sample'+str(sample)],'Hist_EBC_D5_c0_Sample'+str(sample)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBC_D5_c1['Sample'+str(sample)],'Hist_EBC_D5_c1_Sample'+str(sample)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBC_D6_c2['Sample'+str(sample)],'Hist_EBC_D6_c2_Sample'+str(sample)+'_Modulo0-10bins')
+        #plotaHist(tabelao_EBC_D6_c3['Sample'+str(sample)],'Hist_EBC_D6_c3_Sample'+str(sample)+'_Modulo0-10bins')
 
 
 ## Fitting
